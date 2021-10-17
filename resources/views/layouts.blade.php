47afdf482e3b7fd88ce4ca6cfd31bcab45f4b657
Fadil <fadilfdl2710@gmail.com>
2021-10-17 22:52:25 +0700

    Ngetes Tok

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -112,6 +112,7 @@
                     <span>Album</span></a>
             </li>
 
+            {{-- Tes Update --}}
             <!-- Sidebar Toggler (Sidebar) -->
             {{-- <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
