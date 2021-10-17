07ca743f6100e9bf7c020acabff153d87f0cb047
farisqlail <faris.riskilail@gmail.com>
2021-10-17 13:10:12 +0700

    add HomeController and Routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('frontend/index');
-});
+// Route::get('/', function () {
+//     return view('frontend/index');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
