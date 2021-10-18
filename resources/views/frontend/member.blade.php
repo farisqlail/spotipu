5005b3bceddc2c7a683d5e2cc8537682652db699
aprilianina <aprilianinurafifah@gmail.com>
2021-10-18 22:51:18 +0700

    tambah tampilan member

--- resources/views/frontend/member.blade.php
+++ resources/views/frontend/member.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
 
 @section('title')
-    Spotipu - Beli Member
+Spotipu - Beli Member
 @endsection
 
 @section('content')
+<div class="judul" align="center" style="color: #D4D4D4">
+    <h2><b>Dapatkan Akses Premium</b></h2>
+    <h6>Dengarkan musik dengan kualitas tinggi</h6>
+    <h6>dengan harga terjangkau di Spotipu. </h6>
+</div>
+    
+<div class="card-member1">
+    <div class="row">
+        <div class="col-md-3 text-white" style="background-color: #2D3E50; border: none;">
+            <h4><b>Batagor</b></h4>
+            <p >Mulai Rp 5000/hari</p>
+            <hr style="background-color: #ffff;">
+            <p><i class="fas fa-music"></i> Dengarkan musik bebas iklan</p>
+            <p><i class="fas fa-music"></i> Download 30 lagu di perangkat seluler</p>
+            <p><i class="fas fa-music"></i> Buat playlist sesukamu</p>
 
+            <button>Pilih Paket</button>
+
+        </div>
+        <div class="col-md-3"></div>
+        <div class="col-md-3"></div>
+        <div class="col-md-3"></div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdsController;
+use App\Http\Controllers\MemberController;
 
 // Route::get('/', function () {
 //     return view('frontend/index');
