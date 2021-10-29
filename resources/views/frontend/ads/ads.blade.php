1fd538b22e879b2d0a5e808999f2b2785bb4d4a2
farisqlail <faris.riskilail@gmail.com>
2021-10-29 17:56:42 +0700

    form ads done

--- resources/views/frontend/ads/ads.blade.php
+++ resources/views/frontend/ads/ads.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    
+
     <h2 class="text-white">Hello Susi, Welcome back!</h2>
 
     <br>
@@ -16,7 +16,8 @@
         <div class="col-md-3">
             <div class="card rounded" style="background-color: #2D3E50; border: none;">
                 <div class="card-body">
-                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span class="text-white pl-3">Cicil</span>
+                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span
+                        class="text-white pl-3">Cicil</span>
 
                     <p class="mt-3 text-white">
                         Summer season marketing campaign
@@ -29,7 +30,8 @@
         <div class="col-md-3">
             <div class="card rounded" style="background-color: #2D3E50; border: none;">
                 <div class="card-body">
-                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span class="text-white pl-3">Cicil</span>
+                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span
+                        class="text-white pl-3">Cicil</span>
 
                     <p class="mt-3 text-white">
                         Summer season marketing campaign
@@ -42,7 +44,8 @@
         <div class="col-md-3">
             <div class="card rounded" style="background-color: #2D3E50; border: none;">
                 <div class="card-body">
-                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span class="text-white pl-3">Cicil</span>
+                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span
+                        class="text-white pl-3">Cicil</span>
 
                     <p class="mt-3 text-white">
                         Summer season marketing campaign
@@ -55,7 +58,8 @@
         <div class="col-md-3">
             <div class="card rounded" style="background-color: #2D3E50; border: none;">
                 <div class="card-body">
-                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span class="text-white pl-3">Cicil</span>
+                    <img src="{{ asset('assets/img/cicil.png') }}" alt="" srcset=""> <span
+                        class="text-white pl-3">Cicil</span>
 
                     <p class="mt-3 text-white">
                         Summer season marketing campaign
@@ -65,11 +69,11 @@
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
     {{-- end Row Card Cicil --}}
 
     <h5 class="text-white mt-5">Create New Campaign</h5><br>
-    
+
     <form action="" class="text-white form-ads">
         <div class="row">
             <div class="col-md-6">
@@ -101,7 +105,8 @@
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="">Judul Iklan <span style="color:red;">*</span></label>
-                    <textarea name="" class="form-control" id="" cols="30" rows="5"></textarea>
+                    <textarea name="" class="form-control" id="" cols="30"
+                        rows="5">ouetr ini dibuat dari bahan pilihan...</textarea>
                 </div>
             </div>
         </div>
@@ -109,7 +114,7 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="">Harga  <span style="color:red;">*</span></label>
+                    <label for="">Harga <span style="color:red;">*</span></label>
                     <input type="text" name="" class="form-control" placeholder="Rp.00000" id="">
                 </div>
             </div>
@@ -121,6 +126,56 @@
             </div>
         </div>
 
+        <div class="form-group">
+            <label for="">Unggah foto produk <span style="color:red;">*</span></label><br>
+            <button type="button" class="imp-up hvr-shrink" style="background-color: transparent; border: none;">
+                <img src="{{ asset('assets/img/upload-img.png') }}" class="img-fluid" width="160" alt="">
+            </button>
+        </div>
+
+        <h5 class="text-white mt-3">Anggaran & Durasi <span style="color: red;">*</span></h5><br>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="">Anggaran <span style="color:red;">*</span></label>
+                    <input type="text" name="" class="form-control" placeholder="Rp.100000" id="">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="">Durasi <span style="color:red;">*</span></label>
+                    <select class="form-control">
+                        <option>Pilih Durasi</option>
+                        <option>1 Bulan</option>
+                        <option>1 Minggu</option>
+                        <option>3 Hari</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="">Metode Pembayaran <span style="color:red;">*</span></label>
+                    <select class="form-control">
+                        <option>Pilih Metode Pembayaran</option>
+                        <option>Transfer Bank</option>
+                        <option>OVO</option>
+                        <option>Gopay</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="">Durasi <span style="color:red;">*</span></label>
+                    <input type="file" class="form-control" name="">
+                </div>
+            </div>
+        </div>
+
+        <button type="button" class="btn btn-block btn-lg btn-ads hvr-shrink mt-3">Submit</button>
     </form>
 
 @endsection
