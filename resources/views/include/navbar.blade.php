4b44e5b67d4302f7489964b31521a6d2f3fce0a6
farisqlail <faris.riskilail@gmail.com>
2021-10-31 23:57:15 +0700

    pull faris

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -22,8 +22,11 @@
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto" data-aos="zoom-in">
+                   
+                        {{-- <a href="{{ url('/ads') }}" class="btn btn-md text-white mt-3 btn-login hvr-grow">Login</i></a> --}} 
+                        {{-- Button if user doesn't login --}}
 
-                        <a href="{{ url('/ads') }}" class="btn btn-sm text-white mt-3 btn-campaign">&nbsp;<i class="fas fa-plus"> &nbsp; Pasang Iklan &nbsp;</i></a>
+                        <a href="{{ url('/ads') }}" class="btn btn-sm text-white mt-3 btn-campaign hvr-grow">&nbsp;<i class="fas fa-plus"> &nbsp; Pasang Iklan &nbsp;</i></a>
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
                         <li class="nav-item dropdown no-arrow d-sm-none">
